db57c4dcbacd78c4f7e0dac91208b19a622c6a8f
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-06-03 11:24:28 -0300

    BCRYPT JS

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const senha = 'sua_senha';
-const saltRounds = 10;
-
-bcrypt.hash(senha, saltRounds, (err, hash) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Hash da senha:', hash);
-});
 
 // Usuário autorizado (simulado, você pode buscar de um banco de dados)
 const authorizedUser = {
   username: process.env.AUTHORIZED_USERNAME,
-  passwordHash: process.env.AUTHORIZED_PASSWORD_HASH, // Use bcrypt para gerar e armazenar
+  passwordHash: process.env.AUTHORIZED_PASSWORD_HASH, // Use bcryptjs para gerar e armazenar
 };
 
-// Rota única para autenticação e acesso aos dados protegidos
-app.get('/api', async (req, res) => {
+// Middleware de autenticação
+const authenticateUser = async (req, res, next) => {
   const { username, password } = req.query;
 
   // Verifique se o nome de usuário e senha foram fornecidos
@@ -44,38 +34,8 @@ app.get('/api', async (req, res) => {
   // Se as credenciais estiverem corretas, gera um token JWT
   const token = jwt.sign({ username: authorizedUser.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-  // Retorna os dados protegidos junto com o token como resposta
-  res.json({
-    message: 'Login bem-sucedido',
-    token,
-    data: {
-      message: 'Dados protegidos'
-    }
-  });
-});
-
-// Middleware para verificar o token JWT
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) {
-    return res.status(401).json({ error: 'Token não fornecido' });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ error: 'Token inválido' });
-    }
-    req.user = decoded; // Decodificado, podemos acessar o nome de usuário
-    next();
-  });
+  // Adiciona o token à requisição e continua
+  req.token = token;
+  next();
 };
 
-// Middleware de autenticação para todas as rotas protegidas
-app.use(authenticateToken);
-
-// Iniciando o servidor
-app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
-});
