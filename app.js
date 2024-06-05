6345ff8dd1a2130bf15721b3b4ec197f9f709ceb
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-06-05 17:37:02 -0300

    AUTH FOR URL

--- app.js
+++ app.js
@@ -16,39 +16,39 @@ app.use(express.json())
 app.use(cors())
 app.use(routes)
 
-// // Usuário autorizado
-// const authorizedUser = {
-//   username: process.env.AUTHORIZED_USERNAME,
-//   passwordHash: process.env.AUTHORIZED_PASSWORD_HASH,
-// };
+// Usuário autorizado
+const authorizedUser = {
+  username: process.env.AUTHORIZED_USERNAME,
+  passwordHash: process.env.AUTHORIZED_PASSWORD_HASH,
+};
 
-// // Rota única para autenticação e acesso aos dados protegidos
-// app.get('/api', (req, res) => {
-//   const { username, password } = req.query;
+// Rota única para autenticação e acesso aos dados protegidos
+app.get('/api', (req, res) => {
+  const { username, password } = req.query;
 
-//   // Verifique se o nome de usuário e hash da senha foram fornecidos
-//   if (!username || !password) {
-//     return res.status(400).json({ error: 'Favor fornecer nome de usuário e hash da senha' });
-//   }
+  // Verifique se o nome de usuário e hash da senha foram fornecidos
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Favor fornecer nome de usuário e hash da senha' });
+  }
 
-//   // Verifique se o nome de usuário está correto
-//   if (username !== authorizedUser.username) {
-//     return res.status(401).json({ error: 'Credenciais inválidas' });
-//   }
+  // Verifique se o nome de usuário está correto
+  if (username !== authorizedUser.username) {
+    return res.status(401).json({ error: 'Credenciais inválidas' });
+  }
 
-//   // Verifique se o hash da senha está correto
-//   if (password !== authorizedUser.passwordHash) {
-//     return res.status(401).json({ error: 'Credenciais inválidas' });
-//   }
+  // Verifique se o hash da senha está correto
+  if (password !== authorizedUser.passwordHash) {
+    return res.status(401).json({ error: 'Credenciais inválidas' });
+  }
 
-//   // Se as credenciais estiverem corretas, retorne os dados protegidos
-//   res.json({
-//     message: 'Autenticação bem-sucedida',
-//     data: {
-//       message: 'Dados protegidos'
-//     }
-//   });
-// });
+  // Se as credenciais estiverem corretas, retorne os dados protegidos
+  res.json({
+    message: 'Autenticação bem-sucedida',
+    data: {
+      message: 'Dados protegidos'
+    }
+  });
+});
 
 
 async function conecta_db(){
