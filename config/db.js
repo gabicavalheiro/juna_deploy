ce9acbc1a3b2320afe837a46cd026272d682e1cf
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-06-05 15:18:38 -0300

    banco

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ export const sequelize = new Sequelize(
   process.env.MYSQLPASSWORD,
   {
     host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+    port: 3306,
     dialect: 'mysql', // ou 'postgres', 'sqlite', 'mssql', etc., dependendo do seu banco de dados
   }
 );
