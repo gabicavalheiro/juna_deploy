7bfb9b1c20e9da97d089f52baf287e8c94efc0e9
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-12-02 14:58:48 -0300

    testeeee

--- controllers/instagramController.js
+++ controllers/instagramController.js
@@ -1,30 +1,21 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
-
-console.log('Client ID:', process.env.INSTAGRAM_CLIENT_ID);
-console.log('Client Secret:', process.env.INSTAGRAM_CLIENT_SECRET);
-
-
-
-
 
 // Controlador para tratar o redirecionamento do Instagram
 export const instagramAuthCallback = async (req, res) => {
+    const { code } = req.query; // Certifique-se de que a variável `code` é declarada antes de usá-la.
+
     console.log('Recebendo código:', code);
     console.log('Client ID:', process.env.INSTAGRAM_CLIENT_ID);
     console.log('Client Secret:', process.env.INSTAGRAM_CLIENT_SECRET);
     console.log('Redirect URI:', 'https://junadeploy-production.up.railway.app/auth/instagram/callback');
 
-
     // Verifique se as variáveis de ambiente estão configuradas corretamente
     if (!process.env.INSTAGRAM_CLIENT_ID || !process.env.INSTAGRAM_CLIENT_SECRET) {
         return res.status(500).json({ error: 'Variáveis de ambiente não configuradas corretamente' });
     }
-
-    const { code } = req.query;
 
     if (!code) {
         return res.status(400).send('Código de autorização ausente');
